5d8f0f492db5371a5ec211340c19147db63235eb
van <hello@van.cn>
2023-10-31 11:49:03 +1100

    update

--- Docker/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-nohup python3 /home/CS74-1-2023S1/backend/manage.py runserver 0.0.0.0:8000 >> /home/CS74-1-2023S1/run.log 2>&1 &
-service nginx restart
-tail -f /home/CS74-1-2023S1/run.log
